81a6ef56e3780ab4c46087703a5910afb2a32e52
skratchdot <git@skratchdot.com>
2012-11-06 00:22:14 -0500

    Fixing IE bug when rendering Node.DOCUMENT_TYPE_NODE
    Reformatting source code.
    
    Updating version number.

--- domFormat.js
+++ domFormat.js
@@ -1,12 +1,12 @@
 /*!
  * domFormat - A simple javascript library to get DOM nodes as strings.
  * 
- * Release Date: November 04, 2012 [2012-11-04 20:39:19]
- *      Version: 1.0
+ * Release Date: November 06, 2012 [2012-11-06 00:14:51]
+ *      Version: 1.1
  *  Source Code: https://github.com/skratchdot/domFormat  
  *     Examples: http://skratchdot.github.com/domFormat/examples/index-html5.html  
  * 
- * Copyright (c) 2011 SKRATCHDOT.COM
+ * Copyright (c) 2012 SKRATCHDOT.COM
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
@@ -15,7 +15,19 @@
 var domFormat = domFormat || (function () {
 	'use strict';
 
-	var config = {
+	// Declare config variables and functions
+	var config = {}, domFormat = {},
+		// Functions
+		polyfillIsArray, overrideConfig, getNodeName,
+		isVoidElement, isNonIndentedElement, getIndentation, trim,
+		fixWhitespace, htmlSpecialCharacters, formatScript, formatStyle,
+		getNodeString, getElementNode, getAttributeNode, getTextNode,
+		getCdataSectionNode, getEntityReferenceNode, getNodeEntityNode,
+		getProcessingInstructionNode, getCommentNode, getDocumentNode,
+		getDocumentTypeNode, getDocumentFragmentNode, getNotationNode;
+
+	// Setup default config values
+	config = {
 		formatScriptTags : true,
 		formatStyleTags : true,
 		indentString : '\t',
@@ -26,14 +38,25 @@ var domFormat = domFormat || (function () {
 			'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img',
 			'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'
 		]
-	},
+	};
+
+	domFormat.init = function (settings) {
+		overrideConfig(settings, 'indentString', 'string');
+		overrideConfig(settings, 'indentNum', 'number');
+		overrideConfig(settings, 'nonIndentedElements', 'array');
+		overrideConfig(settings, 'voidElements', 'array');
+	};
+
+	domFormat.getString = function (node) {
+		return getNodeString(node, 0);
+	};
 
 	// Polyfill for Array.isArray()
 	polyfillIsArray = function () {
 		Array.isArray = Array.isArray || function (obj) {
 			return {}.toString.call(obj) === '[object Array]';
 		};
-	},
+	};
 
 	overrideConfig = function (settings, prop, type) {
 		// Only override if the property exists in settings
@@ -48,7 +71,7 @@ var domFormat = domFormat || (function () {
 				config[prop] = settings[prop];
 			}
 		}
-	},
+	};
 
 	getNodeName = function (node) {
 		var str = '';
@@ -64,7 +87,7 @@ var domFormat = domFormat || (function () {
 			str += node.nodeName;
 		}
 		return str;
-	},
+	};
 
 	isVoidElement = function (nodeName) {
 		var i = 0;
@@ -79,7 +102,7 @@ var domFormat = domFormat || (function () {
 			}
 		}
 		return false;
-	},
+	};
 
 	isNonIndentedElement = function (nodeName) {
 		var i = 0;
@@ -93,7 +116,7 @@ var domFormat = domFormat || (function () {
 			}
 		}
 		return false;
-	},
+	};
 
 	getIndentation = function (level) {
 		var i = 0, str = '';
@@ -101,15 +124,15 @@ var domFormat = domFormat || (function () {
 			str += config.indentString;
 		}
 		return str;
-	},
+	};
 
 	trim = function (str) {
 		return str.replace(/^\s+|\s+$/g, '');
-	},
+	};
 
 	fixWhitespace = function (str) {
 		return trim(str.replace(/\s+/g, ' '));
-	},
+	};
 
 	htmlSpecialCharacters = function (str, escapeQuotes) {
 		str = str.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
@@ -117,7 +140,7 @@ var domFormat = domFormat || (function () {
 			str = str.replace(/\"/g, '&quot;').replace(/\'/g, '&#039;');
 		}
 		return str;
-	},
+	};
 
 	formatScript = function (str, level) {
 		if (config.formatScriptTags === true) {
@@ -130,7 +153,7 @@ var domFormat = domFormat || (function () {
 			str = str.replace(/\n/g, '\n' + getIndentation(level));
 		}
 		return str;
-	},
+	};
 
 	formatStyle = function (str, level) {
 		if (config.formatStyleTags === true) {
@@ -142,164 +165,218 @@ var domFormat = domFormat || (function () {
 			str = str.replace(/\n/g, '\n' + getIndentation(level));
 		}
 		return str;
-	},
+	};
 
 	getNodeString = function (node, level) {
-		var i = 0, str = '', fixedString = '', nodeType = -1, nodeName = '';
 		if (typeof node === 'object' && typeof node.nodeType === 'number') {
-			nodeType = node.nodeType;
+			if (node.nodeType === 1) {
+				// Node.ELEMENT_NODE === 1
+				return getElementNode(node, level);
+			} else if (node.nodeType === 2) {
+				// Node.ATTRIBUTE_NODE === 2
+				return getAttributeNode(node, level);
+			} else if (node.nodeType === 3) {
+				// Node.TEXT_NODE === 3
+				return getTextNode(node, level);
+			} else if (node.nodeType === 4) {
+				// Node.CDATA_SECTION_NODE === 4
+				return getCdataSectionNode(node, level);
+			} else if (node.nodeType === 5) {
+				// Node.ENTITY_REFERENCE_NODE === 5
+				return getEntityReferenceNode(node, level);
+			} else if (node.nodeType === 6) {
+				// Node.ENTITY_NODE === 6
+				return getNodeEntityNode(node, level);
+			} else if (node.nodeType === 7) {
+				// Node.PROCESSING_INSTRUCTION_NODE === 7
+				return getProcessingInstructionNode(node, level);
+			} else if (node.nodeType === 8) {
+				// Node.COMMENT_NODE === 8
+				return getCommentNode(node, level);
+			} else if (node.nodeType === 9) {
+				// Node.DOCUMENT_NODE === 9
+				return getDocumentNode(node, level);
+			} else if (node.nodeType === 10) {
+				// Node.DOCUMENT_TYPE_NODE === 10
+				return getDocumentTypeNode(node, level);
+			} else if (node.nodeType === 11) {
+				// Node.DOCUMENT_FRAGMENT_NODE === 11
+				return getDocumentFragmentNode(node, level);
+			} else if (node.nodeType === 12) {
+				// Node.NOTATION_NODE === 12
+				return getNotationNode(node, level);
+			}
 		}
-		switch (nodeType) {
-// Node.ELEMENT_NODE === 1
-		case 1:
-			nodeName = getNodeName(node);
-			// Print a newline if we are deeper than level 0
-			if (level > 0) {
-				str += '\n';
+		return '';
+	};
+
+	// Node.ELEMENT_NODE === 1
+	getElementNode = function (node, level) {
+		var str = '', nodeName, i;
+		// store the name of our element
+		nodeName = getNodeName(node);
+		// Print a newline if we are deeper than level 0
+		if (level > 0) {
+			str += '\n';
+		}
+		// If this is a non-indented element, set level to 0
+		if (isNonIndentedElement(nodeName)) {
+			level = 0;
+		}
+		// Now print some tabs
+		str += getIndentation(level);
+		// Start opening the tag
+		str += '<' + nodeName;
+		// Print the attributes
+		for (i = 0; i < node.attributes.length; i++) {
+			str += getNodeString(node.attributes[i], level + 1);
+		}
+		// If there are no children, we can close the tag
+		if (node.childNodes.length === 0) {
+			// Void elements can use the "self-closing" form.
+			if (isVoidElement(nodeName)) {
+				str += '/>';
+			} else {
+				str += '></' + nodeName + '>';
 			}
-			// If this is a non-indented element, set level to 0
-			if (isNonIndentedElement(nodeName)) {
-				level = 0;
-			}
-			// Now print some tabs
-			str += getIndentation(level);
-			// Start opening the tag
-			str += '<' + nodeName;
-			// Print the attributes
-			for (i = 0; i < node.attributes.length; i++) {
-				str += getNodeString(node.attributes[i], level + 1);
-			}
-			// If there are no children, we can close the tag
-			if (node.childNodes.length === 0) {
-				// Void elements can use the "self-closing" form.
-				if (isVoidElement(nodeName)) {
-					str += '/>';
-				} else {
-					str += '></' + nodeName + '>';
-				}
-			} else if (node.childNodes.length === 1 && 
+		} else if (node.childNodes.length === 1 &&
 				node.childNodes[0].nodeType === 3 &&
 				nodeName.toLowerCase() !== 'script' &&
-				nodeName.toLowerCase() !== 'style'
-			) {
-				// If the only child is a text node, then don't use newlines
-				str += '>' + trim(getNodeString(node.childNodes[0], level + 1)) + '</' + nodeName + '>';
-			} else {
-				str += '>';
-				for (i = 0; i < node.childNodes.length; i++) {
-					str += getNodeString(node.childNodes[i], level + 1);
-				}
-				str += '\n' + getIndentation(level) + '</' + nodeName + '>';
-			}
-			break;
-// Node.ATTRIBUTE_NODE === 2
-		case 2:
-			// if statement is needed due to IE adding attributes like onresizeend and style
-			if (node.specified === true && node.nodeValue !== null) {
-				str += ' ' + 
-					node.nodeName +
-					'="' +
-					htmlSpecialCharacters(node.nodeValue, true) +
-					'"';
-			}
-			break;
-// Node.TEXT_NODE === 3
-		case 3:
-			if (node.parentNode.nodeName.toLowerCase() === 'script') {
-				str += formatScript(node.nodeValue, level);
-			} else if (node.parentNode.nodeName.toLowerCase() === 'style') {
-				str += formatStyle(node.nodeValue, level);
-			} else {
-				fixedString = fixWhitespace(node.nodeValue);
-				if (fixedString.length > 0) {
-					str += '\n' + getIndentation(level) + htmlSpecialCharacters(fixedString, false);
-				}
-			}
-			break;
-// Node.CDATA_SECTION_NODE === 4
-		case 4:
-			if (node.parentNode.nodeName.toLowerCase() === 'script') {
-				str += '<[CDATA[' + formatScript(node.nodeValue, level) + ']]>';
-			} else if (node.parentNode.nodeName.toLowerCase() === 'style') {
-				str += '<[CDATA[' + formatStyle(node.nodeValue, level) + ']]>';
-			} else {
-				fixedString = fixWhitespace(node.nodeValue);
-				if (fixedString.length > 0) {
-					str += '\n' + getIndentation(level) + '<[CDATA[' + fixedString + ']]>';
-				}
-			}
-			break;
-// Node.ENTITY_REFERENCE_NODE === 5
-		case 5:
-			str = '';
-			break;
-// Node.ENTITY_NODE === 6
-		case 6:
-			str = '';
-			break;
-// Node.PROCESSING_INSTRUCTION_NODE === 7
-		case 7:
-			str = '';
-			break;
-// Node.COMMENT_NODE === 8
-		case 8:
-			// Fix for IE treating DOCTYPE node as a comment.
-			if (level > 0) {
-				str += '\n' + getIndentation(level);
-			}
-			str += '<!--' + node.nodeValue + '-->';
-			if (level === 0) {
-				str += '\n';
-			}
-			break;
-// Node.DOCUMENT_NODE === 9
-		case 9:
+				nodeName.toLowerCase() !== 'style') {
+			// If the only child is a text node, then don't use newlines
+			str += '>' + trim(getNodeString(node.childNodes[0], level + 1)) + '</' + nodeName + '>';
+		} else {
+			str += '>';
 			for (i = 0; i < node.childNodes.length; i++) {
-				str += getNodeString(node.childNodes[i], level);
+				str += getNodeString(node.childNodes[i], level + 1);
 			}
-			break;
-// Node.DOCUMENT_TYPE_NODE === 10
-		case 10:
-			str += '<!DOCTYPE';
-			if (node.nodeName.length > 0) {
-				str += ' ' + node.nodeName;
-			}
-			if (node.publicId.length > 0) {
-				str += ' PUBLIC "' + node.publicId + '"';
-			}
-			if (node.systemId.length > 0) {
-				str += ' "' + node.systemId + '"';
-			}
-			str += '>\n';
-			break;
-// Node.DOCUMENT_FRAGMENT_NODE === 11
-		case 11:
-			for (i = 0; i < node.childNodes.length; i++) {
-				str += getNodeString(node.childNodes[i], level);
-			}
-			break;
-// Node.NOTATION_NODE === 12
-		case 12:
-			str = '';
-			break;
+			str += '\n' + getIndentation(level) + '</' + nodeName + '>';
 		}
 		return str;
 	};
 
-	return {
-
-		init : function (settings) {
-			overrideConfig(settings, 'indentString', 'string');
-			overrideConfig(settings, 'indentNum', 'number');
-			overrideConfig(settings, 'nonIndentedElements', 'array');
-			overrideConfig(settings, 'voidElements', 'array');
-		},
-
-		getString : function (node) {
-			return getNodeString(node, 0);
+	// Node.ATTRIBUTE_NODE === 2
+	getAttributeNode = function (node, level) {
+		var str = '';
+		// if statement is needed due to IE adding attributes like onresizeend and style
+		if (node.specified === true && node.nodeValue !== null) {
+			str += ' ' +
+				node.nodeName +
+				'="' +
+				htmlSpecialCharacters(node.nodeValue, true) +
+				'"';
 		}
-
+		return str;
 	};
+
+	// Node.TEXT_NODE === 3
+	getTextNode = function (node, level) {
+		var str = '', fixedString;
+		if (node.parentNode.nodeName.toLowerCase() === 'script') {
+			str += formatScript(node.nodeValue, level);
+		} else if (node.parentNode.nodeName.toLowerCase() === 'style') {
+			str += formatStyle(node.nodeValue, level);
+		} else {
+			fixedString = fixWhitespace(node.nodeValue);
+			if (fixedString.length > 0) {
+				str += '\n' + getIndentation(level) + htmlSpecialCharacters(fixedString, false);
+			}
+		}
+		return str;
+	};
+
+	// Node.CDATA_SECTION_NODE === 4
+	getCdataSectionNode = function (node, level) {
+		var str = '', fixedString;
+		if (node.parentNode.nodeName.toLowerCase() === 'script') {
+			str += '<[CDATA[' + formatScript(node.nodeValue, level) + ']]>';
+		} else if (node.parentNode.nodeName.toLowerCase() === 'style') {
+			str += '<[CDATA[' + formatStyle(node.nodeValue, level) + ']]>';
+		} else {
+			fixedString = fixWhitespace(node.nodeValue);
+			if (fixedString.length > 0) {
+				str += '\n' + getIndentation(level) + '<[CDATA[' + fixedString + ']]>';
+			}
+		}
+		return str;
+	};
+
+	// Node.ENTITY_REFERENCE_NODE === 5
+	getEntityReferenceNode = function (node, level) {
+		return '';
+	};
+
+	// Node.ENTITY_NODE === 6
+	getNodeEntityNode = function (node, level) {
+		return '';
+	};
+
+	// Node.PROCESSING_INSTRUCTION_NODE === 7
+	getProcessingInstructionNode = function (node, level) {
+		return '';
+	};
+
+	// Node.COMMENT_NODE === 8
+	getCommentNode = function (node, level) {
+		var str = '';
+		// Fix for IE treating DOCTYPE node as a comment.
+		if (level > 0) {
+			str += '\n' + getIndentation(level);
+		}
+		if (node.nodeValue) {
+			str += '<!--' + node.nodeValue + '-->';
+		}
+		if (level === 0) {
+			str += '\n';
+		}
+		return str;
+	};
+
+	// Node.DOCUMENT_NODE === 9
+	getDocumentNode = function (node, level) {
+		var str = '', i;
+		if (node.childNodes && node.childNodes.length) {
+			for (i = 0; i < node.childNodes.length; i++) {
+				str += getNodeString(node.childNodes[i], level);
+			}
+		}
+		return str;
+	};
+
+	// Node.DOCUMENT_TYPE_NODE === 10
+	getDocumentTypeNode = function (node, level) {
+		var str = '';
+		str += '<!DOCTYPE';
+		if (node.nodeName && node.nodeName.length > 0) {
+			str += ' ' + node.nodeName;
+		}
+		if (node.publicId && node.publicId.length > 0) {
+			str += ' PUBLIC "' + node.publicId + '"';
+		}
+		if (node.systemId && node.systemId.length > 0) {
+			str += ' "' + node.systemId + '"';
+		}
+		str += '>\n';
+		return str;
+	};
+
+	// Node.DOCUMENT_FRAGMENT_NODE === 11
+	getDocumentFragmentNode = function (node, level) {
+		var str = '', i;
+		if (node.childNodes && node.childNodes.length) {
+			for (i = 0; i < node.childNodes.length; i++) {
+				str += getNodeString(node.childNodes[i], level);
+			}
+		}
+		return str;
+	};
+
+	// Node.NOTATION_NODE === 12
+	getNotationNode = function (node, level) {
+		return '';
+	};
+
+	return domFormat;
 }());
 /*!
  * Slightly modified version of CSS Beautify:
